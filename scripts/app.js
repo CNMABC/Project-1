4d2f8d8590af47bc88984e38a0a731328eb3eaa0
Cecilia Nunn <cecilia.nunn@gmail.com>
2021-05-28 12:54:25 +0100

    v1 characters

---
+++ scripts/app.js
@@ -0,0 +1,5 @@
+function init() {
+  console.log('js up and running')
+  
+}
+window.addEventListener('DOMContentLoaded', init)
---
+++ scripts/notes.js
@@ -0,0 +1,43 @@
+
+//Space invaders
+// Grid : Create a grid for the invasion game 
+
+//? Characters 
+// Main player spaceship --- create - Java?
+// spaceship can only go from left to right 
+// create variable for it in JS
+// decide the gif for the spaceship
+
+// Moving Aliens : create the block rows of aliens 
+// Create X amount of aliens in a block (? x ?)
+// Decide picture for the aliens
+// on a timer they move down by one row every X seconds
+// they shoot 
+// create a variable for it in JS
+
+//? Story of Game invaders
+// press start button to go 
+// button pressed - game begins -----------> 
+// spaceship is present on the grid & aliens 
+// listens for direction from keys whether to move left or right 
+// space bar or up arrow enables ship to shoot
+// aliens start moving from left to right 
+// if spaceship shooting  === alien - alien closest to spaceship is removed from that column (how?)
+// if no alien left in column = null (nothing happens) 
+// spaceship shoot in straight direction '(-)up' at the spaceship (directed by keys)
+// random aliens shoot in straight direction '(+)down' at the spaceship (time intervals)
+// if alien shooting === spaceship - restart game 
+// Enable game to re-start 10 times before game over 
+// pop-up with message "Game Over!" 
+// every X seconds alien block moves down by 1 row on the grid
+// if alien block === alien row = restart the game 
+// at end of game players score is displayed 
+// ---- make mobile responsive 
+
+
+//? Styling : 
+// ---- Create styling/image for the background colour
+// ---- Choose font type for the game
+// ---- Create heading for title 
+
+// !----------------------------
