804ff424012ba492d6f6a2df45f682bee8498314
Cecilia Nunn <cecilia.nunn@gmail.com>
2021-06-01 23:10:38 +0100

    CSS, moving giants, near bullets

--- scripts/app.js
+++ scripts/app.js
@@ -9,20 +9,21 @@ function init() {
   const startingGretaPosition = 94 
   let currentGretaPosition = 94 
   const gretaClass = 'greta'
-
-
   const startingGiantPosition = [1,2,3,4,5,6,7,8,11,12,13,14,15,16,17,18,21,22,23,24,25,26,27,28,31,32,33,34,35,36,37,38] 
   let currentGiantPosition = [1,2,3,4,5,6,7,8,11,12,13,14,15,16,17,18,21,22,23,24,25,26,27,28,31,32,33,34,35,36,37,38,] 
   const giantClass = 'giant'
-
-  const startingTreePosition = 84 
-  let currentTreePosition = 84  
-  const treesClass = 'trees'
-
+  let giantDirection = 'right' // variable to keep track of what direction the giants are travelling in, starting with right
+  let giantTimer // variable to keep track of the timer id for the giants, can use this to clear the interval when its game over
+  
+  const startingTreePosition = currentGretaPosition
+  const currentTreePosition = currentGretaPosition
+  console.log('current tree position',currentTreePosition)
+  console.log('starting tree position',startingTreePosition)
+  const treeClass = 'trees'
   const startingOilPosition = 55 
   let currentOilPosition = 55 
   const oilClass = 'oil'
-
+  const giantsHitEdge = [80,81,82,83,84,85,86,87,88,89]
 
   // !updating sessions
   const startButton = document.querySelector('#start-button')
@@ -32,23 +33,23 @@ function init() {
 // ! Timers ----------------------------
   let treesTimer
   let oilTimer
-  let livesRemaining = 10  
-  let score = 0 
-  let moveRight = true
+  const livesRemaining = 10  
+  const score = 0 
+  const moveRight = true
 
   //! Make the grid ---------------------
   function createGrid(startingGretaPosition) {
     for (let i = 0; i < cellCount; i++) { // keep going as long as i is less than the cell count
-      
       const cell = document.createElement('div')// created a div - similar to query selector 
       cell.innerText = i
       grid.appendChild(cell)// passed in cell element newly created - all divs are now children
       cells.push(cell)
     }
     addGreta(startingGretaPosition)// leave this here 
-    addTrees(startingTreePosition)
-    addOil(startingOilPosition)
     addGiants(startingGiantPosition)
+    addTree(startingTreePosition)// remove this as need to invoke them being added when game starts 
+    // addOil(startingOilPosition)
+    
   }
   
   
@@ -57,8 +58,8 @@ function init() {
     cells[position].classList.add('greta')
   }
 
-  function addTrees(position){
-    cells[position].classList.add('tress')
+  function addTree(position){
+    cells[position].classList.add('trees')
   }
   function addOil(position){
     cells[position].classList.add('oil')
@@ -83,15 +84,21 @@ function init() {
     cells[position].classList.remove('oil')
   } 
 
-  function removeGiants(position) {// this would remove all giants so this needs changing 
-    cells[position].classList.remove('giant')
+  function removeGiants(positions) {// this would remove all giants so this needs changing 
+    positions.forEach(position => {
+      cells[position].classList.remove('giant')
+    })
   }
 
 
-// ! Get greta to move left & right with arrows
+// ! -------------------------Get greta to move left & right with arrows ------------
   function moveGreta(event) {
     console.log('current location of greta',currentGretaPosition)
     const key = event.keyCode
+    if (key === 32){
+      event.preventDefault()
+      console.log('key is being pressed-->',moveTree)// change to function moving the laser - invoke function here 
+    }
     removeGreta(currentGretaPosition)
 
     if (key === 39 && currentGretaPosition % width !== width - 1) {// if the key is equal to 39 (right arrow)
@@ -109,57 +116,112 @@ function init() {
   document.addEventListener('keyup', moveGreta)  
   createGrid(startingGretaPosition)
   
-//! Get Giants to move 
+//! -----------------------------------Get Giants to move ----------------------------
   function startGame() {
-    addGiants(startingGiantPosition)
-    console.log(startGame)
-}
+  addGiants(startingGiantPosition)
+  moveGiants()
+  // call the function oilDrop to start 
+  }
 
-  // function moveGiants() {
-  //   removeGiants(currentGiantPosition)
-  //   const giantNewPosition = currentGiantPosition.map(giantItem => {
-  //     const right = currentGiantPosition++
-  //     return right
-  //     if (currentGiantPositions = currentGiantPositions.map(position => position++)){
-  //     console.log('changes')
-  //     } else if (leftBorder ) { 
-  //       const left = currentGiantPosition--
-  //       return left 
-  //   }
-  //   })
+  function moveGiants() {
+    giantTimer = setInterval(() => {// call the function once and it will trigger the interval once (just in case we need to add more logic before or after)
+      removeGiants(currentGiantPosition)
+      const giantsOnRightEdge = currentGiantPosition.some(giant => giant % width === 9)
+      const giantsOnLeftEdge = currentGiantPosition.some(giant => giant % width === 0)
+      const giantsOnBottomEdge = currentGiantPosition.some(giant => giantsHitEdge.includes(giant))
+      console.log('giants have hit-->',giantsOnBottomEdge)
+      if (giantsOnBottomEdge) {
+        return clearInterval(giantTimer)// don't run anything else in this function (just stop!) 
+      // return gameover 
+      }
+      if (giantDirection === 'right') {
+        if (giantsOnRightEdge) {
+          currentGiantPosition = currentGiantPosition.map(giant => giant + width)
+          giantDirection = 'left'
+        } else { // if they're not on the edge then move them right as normal
+          currentGiantPosition = currentGiantPosition.map(giant => giant + 1)
+        }
+      } else if (giantDirection === 'left') {
+        if (giantsOnLeftEdge) {
+          currentGiantPosition = currentGiantPosition.map(giant => giant + width)
+          giantDirection = 'right'
+        } else {
+          currentGiantPosition = currentGiantPosition.map(giant => giant - 1)
+        }
+      }
+      addGiants(currentGiantPosition)
+    }, 1500)
+  }
+// ------------------- coding area -------------------
+// function addTree(position){
+//   cells[position].classList.add('trees')
 // }
-//     
 
-
-//       for (let i = 0; i < currentGiantPosition.length; i ++) {
-//         const giantRight = currentGiantPosition[i] ++ 
-//       }
-//       for (let i = 0; i < currentGiantPosition.length; i ++) {
-//       const giantLeft = currentGiantPosition[i] -- 
-//       }
-//       if (currentGiantPositions = currentGiantPositions.map(position => position++){}
-
-//     })
+// function removeTrees(position){
+//   cells[position].classList.remove('trees')
 // }
 
+  function moveTree() {
+    console.log('current location of tree ---->',startingTreePosition)
+    treesTimer = setInterval(() => {
+      removeTrees(currentGretaPosition)
+      const newTreePosition = currentGretaPosition- width
+      console.log('the trees are here',currentTreePosition)
+      addTree(currentGretaPosition)
+    }, 6000)
+    
+  }
 
 
-  // 2. redefine their positions 
-  // this is redefining the currentGiantPositions array to be equal to the result of the map
-  // the map is looping through the current positions, increasing each index by one
-  // and returning a new array of indexes. ++ to move right -- to move left
-  // you'll need to also keep track of their direction (in a variable starting as 'right')
-  // add an if statement that checks the direction and redefines the indexes based on that
-  // currentGiantPositions = currentGiantPositions.map(position => position++)
-  // log currentGiantPositions here and see how its changing
-  // 3. add them back at their new positions, passing in currentGiantPositions
-// }
-// // 4. call the moveGiants function on an interval so it happens every 1 second or whatever
-
-removeGiants(currentGiantPosition)
-setInterval(moveGiants, 3000)
+  console.log('a tree is being added', addTree)
+  console.log('a tree is being removed',removeTrees)
+  moveTree()
 
 
+
+
+
+
+// // function oilDrop ()
+//   oilTimer = setInterval(() => {
+
+// }, 1500)
+// when the start button is clicked (event listener)get oil image to do this function 
+// from startingOilPosition randomly release images to drop down 
+// check if gone off grid - clear the interval 
+
+
+
+
+
+
+
+
+// when the start button is clicked start this function 
+// when this function is called every 5 seconds I want it to randomly drop bullets across the page 
+// from that location every 5 seconds randomly drop bullets down
+// make bullets go in a straight line 
+
+
+
+
+
+
+// if alien shot remove alien from that location 
+
+// Greta 
+// shoot from above her current location in a straight line with space bar
+// re-start game everytime greta dies 
+// after 3 lives dead start from the beginning   
+
+
+// Score 
+// Everytime an alien dies +10 
+// everytime Greta gets hit lose a life 
+
+//?-----------------------------------------------
+
+startButton.addEventListener('click',startGame)
 }
 
 window.addEventListener('DOMContentLoaded', init)
--- scripts/notes.js
+++ scripts/notes.js
@@ -41,3 +41,151 @@
 // ---- Create heading for title 
 
 // !----------------------------
+
+function init() {
+  console.log('js up and running')
+  const grid = document.querySelector('.grid')
+  const width = 10
+  const cellCount = width * width
+  const cells = [] // empty array - so you can keep track of them as can't select from html - need to store them
+  // ! ----Characters in the game -----------------------------------------
+  const startingGretaPosition = 94 
+  let currentGretaPosition = 94 
+  const gretaClass = 'greta'
+  const startingGiantPosition = [2,3,4,5,6,7,12,13,14,15,16,17,22,23,24,25,26,27,32,33,34,35,36,37] 
+  let currentGiantPosition = [2,3,4,5,6,7,12,13,14,15,16,17,22,23,24,25,26,27,32,33,34,35,36,37] 
+  const giantClass = 'giant'
+  let giantDirection = 'right' // variable to keep track of what direction the giants are travelling in, starting with right
+   // variable to keep track of the timer id for the giants, can use this to clear the interval when its game over
+  const startingTreePosition = currentGretaPosition 
+  console.log('starting tree position',startingTreePosition)
+  const treesClass = 'trees'
+  const startingOilPosition = 55 
+  const currentOilPosition = 55 
+  const oilClass = 'oil'
+  // !updating sessions
+  const startButton = document.querySelector('#start-button')
+  const scoreDisplay = document.querySelector('#score-display')
+  const livesDisplay = document.querySelector('#lives-display') 
+  // ! Timers ----------------------------
+  let giantTimer
+  let treesTimer
+  let oilTimer
+  const livesRemaining = 10  
+  const score = 0 
+  const moveRight = true
+  //! Make the grid ---------------------
+  function createGrid(startingGretaPosition) {
+    for (let i = 0; i < cellCount; i++) { // keep going as long as i is less than the cell count
+      const cell = document.createElement('div')// created a div - similar to query selector 
+      cell.innerText = i
+      grid.appendChild(cell)// passed in cell element newly created - all divs are now children
+      cells.push(cell)
+    }
+    // addGreta(startingGretaPosition)// leave this here 
+    // addTrees(startingTreePosition)
+    // addOil(startingOilPosition)
+    // addGiants(startingGiantPosition)
+  }
+  //! Add characters to the page -------------
+  function addGreta(position){
+    cells[position].classList.add('greta')
+  }
+  function addTrees(position){
+    cells[position].classList.add('tress')
+  }
+  function addOil(position){
+    cells[position].classList.add('oil')
+  } 
+  function addGiants(positions) {
+    positions.forEach(position => {
+      cells[position].classList.add('giant')
+    })
+  }
+  //! Remove characters from the page ---------
+  function removeGreta(position) {
+    cells[position].classList.remove('greta')
+  }
+  function removeTrees(position){
+    cells[position].classList.remove('trees')
+  }
+  function removeOil(position){
+    cells[position].classList.remove('oil')
+  } 
+  function removeGiants(positions) {// this would remove all giants so this needs changing 
+    positions.forEach(position => {
+      cells[position].classList.remove('giant')
+    })
+  }
+  // ! ----------------Get greta to move left & right with arrows----------------
+  function moveGreta(event) {
+    // console.log('current location of greta',currentGretaPosition)
+    const key = event.keyCode
+    removeGreta(currentGretaPosition)
+    if (key === 39 && currentGretaPosition % width !== width - 1) {// if the key is equal to 39 (right arrow)
+      console.log('RIGHT')// if right increases by one 
+      currentGretaPosition++// move the position of the cat to be one more than it currently is 
+    } else if (key === 37 && currentGretaPosition % width !== 0) { // making sure this is responding to left - checking ig current position of cat is equal to 9  
+      console.log('LEFT')
+      currentGretaPosition--
+    } else {
+      console.log('INVALID KEY')
+    }
+    addGreta(currentGretaPosition)
+  }
+  document.addEventListener('keyup', moveGreta)  
+  createGrid(startingGretaPosition)
+
+  //! ----------------------Get Giants to move ----------------
+  function startGame() {
+    addGiants(startingGiantPosition)
+    moveGiants() // call the moveGiants function when the start button is clicked
+  }
+  function moveGiants() {
+    giantTimer = setInterval(() => { // set interval so this functionality runs every 1.5s to move the giants
+      // remove giants from their current positions
+      removeGiants(currentGiantPosition)
+      // check if the giants are on the right edge, value of variable will be true or false
+      // this is checking if the any of the giants positions divided by width leaves a remainder of 9, if its true then we've hit the edge
+      const giantsOnRightEdge = currentGiantPosition.some(giant => giant % width === 9)
+      // check if the giants are on the left edge, same as above but for the opposite edge
+      const giantsOnLeftEdge = currentGiantPosition.some(giant => giant % width === 0)
+      if (giantDirection === 'right') { // check direction giants are moving
+        if (giantsOnRightEdge) { // if the are on the edge, move them down using + width and change their direction to left
+          currentGiantPosition = currentGiantPosition.map(giant => giant + width)
+          giantDirection = 'left'
+        } else { // if they're not on the edge then move them right as normal
+          currentGiantPosition = currentGiantPosition.map(giant => giant + 1)
+        }
+      } else if (giantDirection === 'left') { // check if the giants are moving left
+        if (giantsOnLeftEdge) { // if they are on the left edge then move them down and change the direction to right
+          currentGiantPosition = currentGiantPosition.map(giant => giant + width)
+          giantDirection = 'right'
+        } else { // if they're not on the left edge, keep moving them left as normal
+          currentGiantPosition = currentGiantPosition.map(giant => giant - 1)
+        }
+      }
+      // add them back at their new position
+      addGiants(currentGiantPosition)
+    }, 1500)
+  }
+  // ? to get the laser working
+  // variable for laserPosition, this should start equal to greta position so its always where she is on the page
+  // function to add the laser
+  // function to remove the laser
+  // function to move the laser
+  // inside the move laser function add an interval
+  // inside the interval you want to remove the laser
+  // then redefine the position of the laser, minusing width each time so it moves up
+  // add the laser back at the new position
+  // invoke this function on whatever key you want, add a key event listener and that should call the moveLaser function
+  // add in a condition to stop the timer if the laser goes off the top of the grid, think about the condition you can use from the cat game to check this
+  // next add a check inside moveLaser to check if there has been a collision, does the div with the class of laser also have a class of giant?
+  // if there is a collision, remove that index from the currentGiantPosition array so the giant is dead and wont come back
+  // the last step above can be broken down further so just take it step by step
+  // for the giants you will also want a check at some point to stop them going off the bottom of the grid, similar logic to checking if they are on the edge
+  
+
+  startButton.addEventListener('click', startGame)
+}
+window.addEventListener('DOMContentLoaded', init)
